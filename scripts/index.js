fe0abf1c2180fb54133906ebae9b1b89229ecec5
Tony <32371862+sktonyca@users.noreply.github.com>
2020-03-08 12:48:51 -0400

    updated index.html

--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,17 @@ function button_click(x) {
 
     if (x == "about") {
       document.getElementById("aboutArea").style.display = "block";
+      document.getElementById("projectsArea").style.display = "none";
+    }
+
+    if (x == "projects") {
+      document.getElementById("aboutArea").style.display = "none";
+      document.getElementById("projectsArea").style.display = "block";
     }
   } else {
     console.log(x);
     document.getElementById("logo").style.display = "block";
     document.getElementById("aboutArea").style.display = "none";
-    start();
+    document.getElementById("projectsArea").style.display = "none";
   }
 }
