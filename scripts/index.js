8101564049563adafe906be3575b3390cec063ce
Tony <32371862+sktonyca@users.noreply.github.com>
2020-03-10 10:44:52 -0400

    updated index.html

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,30 @@
+// Nav menu click event
 function button_click(x) {
   if (x != "home") {
     document.getElementById("logo").style.display = "none";
 
     if (x == "about") {
-      document.getElementById("aboutArea").style.display = "block";
       document.getElementById("projectsArea").style.display = "none";
+      document.getElementById("contactArea").style.display = "none";
+      document.getElementById("aboutArea").style.display = "block";
     }
 
     if (x == "projects") {
       document.getElementById("aboutArea").style.display = "none";
+      document.getElementById("contactArea").style.display = "none";
       document.getElementById("projectsArea").style.display = "block";
+    }
+
+    if (x == "contact") {
+      document.getElementById("aboutArea").style.display = "none";
+      document.getElementById("projectsArea").style.display = "none";
+      document.getElementById("contactArea").style.display = "block";
     }
   } else {
     console.log(x);
-    document.getElementById("logo").style.display = "block";
     document.getElementById("aboutArea").style.display = "none";
     document.getElementById("projectsArea").style.display = "none";
+    document.getElementById("contactArea").style.display = "none";
+    document.getElementById("logo").style.display = "block";
   }
 }
